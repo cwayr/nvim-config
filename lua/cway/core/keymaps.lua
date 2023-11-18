01ba652734a2877ab98e749870dd2a046f178c89
Caleb Waymeyer <60524606+cwaymeyer@users.noreply.github.com>
2023-11-17 22:10:27 -0500

    update key mappings

--- lua/cway/core/keymaps.lua
+++ lua/cway/core/keymaps.lua
@@ -3,8 +3,7 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
 
----------------------
--- General Keymaps -------------------
+------------------- General Keymaps -------------------
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
@@ -15,16 +14,24 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
 
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
-
 -- window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>so", "<cmd>only<CR>", { desc = "Close all splits except current" }) -- close all splits except current
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sn", "<C-w>w", { desc = "Go to next split" }) -- go to next split
 
+-- file explorer
+keymap.set("n", "<leader>sf", ":Vexplore | :vertical resize 50<CR>", { desc = "Open file explorer" }) -- open file explorer
+
+-- terminal
+keymap.set("n", "<leader>st", ":vsp | terminal<CR> | i", { desc = "Open terminal" }) -- open terminal
+keymap.set("t", "<leader>tn", "<C-\\><C-n>", { desc = "Exit i mode (terminal)" }) -- exit terminal
+keymap.set("t", "<leader>sn", "<C-\\><C-n><C-w>w", { desc = "Go to next split (terminal)" }) -- go to next split (terminal)
+keymap.set("t", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split (terminal)" }) -- close current split (terminal)
+
+-- tab management
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
 keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
